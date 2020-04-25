609f487fb8a4abca084d0389d2a597d4ff748c63
Karsh <karshie04@gmail.com>
2020-04-25 18:47:00 +0100

    ready to deploy

--- src/Pages/Schedule.js
+++ src/Pages/Schedule.js
@@ -1,3 +1,25 @@
+import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+ 
+const localizer = momentLocalizer(moment)
+ 
+const MyCalendar = props => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={[]}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+)
+
+export default MyCalendar
+
+
 // import React, {Component} from "react";
 
 // class Schedule extends Component {
